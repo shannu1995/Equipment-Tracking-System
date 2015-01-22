3e86c3881fb623b8644c08941ab21006560f4e6d
Shannu <s3453738@student.rmit.edu.au>
2015-01-22 11:59:20 +1100

    added name

--- ets_options.h
+++ ets_options.h
@@ -1,10 +1,10 @@
 /***********************************************************************
  * COSC1076 - Advanced Programming Techniques
  * Summer 2015 Assignment #2
- * Full Name        : EDIT HERE
- * Student Number   : EDIT HERE
- * Course Code      : EDIT HERE
- * Program Code     : EDIT HERE
+ * Full Name        : Shanmuka Prasad Sivapurapu
+ * Student Number   : s3453738
+ * Course Code      : COSC 1076
+ * Program Code     : BP   0096
  * Start up code provided by David Shaw
  * Based on 2014 code by Paul Miller and Virginia King
  **********************************************************************/
